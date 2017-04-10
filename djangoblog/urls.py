3f04675626e2693e2bcd0ee18f31901629733353
Adriana Matsufuji <adrianabm@gmail.com>
2017-04-10 16:22:51 +0200

    Create PostDetail view and template

--- djangoblog/urls.py
+++ djangoblog/urls.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 
-from posts.views import HomeView
+from posts.views import HomeView, PostDetailView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', HomeView, name='home')
+    url(r'^$', HomeView, name='home'),
+    url(r'^posts/(?P<post_id>[0-9]+)/$', PostDetailView, name='post_detail')
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Post
 
 def HomeView(request):
-    posts = Post.objects.order_by('pub_date')
-
+    posts = Post.objects.order_by('-pub_date')
     return render(request, 'posts/home.html', { 'posts': posts })
+
+def PostDetailView(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'posts/post_detail.html', { 'post': post })
