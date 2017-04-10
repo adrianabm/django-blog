58b8c94a7b0a9abcb3a6bd4b277b35e07d2f72ef
Adriana Matsufuji <adrianabm@gmail.com>
2017-04-10 15:59:08 +0200

    Add setup to display images correctly

--- djangoblog/urls.py
+++ djangoblog/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 from posts.views import HomeView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', HomeView, name='home')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
